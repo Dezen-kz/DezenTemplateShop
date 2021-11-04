faaacb5a3a372741e759b4eb5d4c7cf9c873f590
dilmuratserik <62326731+dilmuratserik@users.noreply.github.com>
2021-11-04 18:50:16 +0600

    Project

---
+++ config/services.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'github' => [
+        'client_id' => '803427c290421b9fad77', //Github API
+        'client_secret' => 'a063b11c0371fd1576f4e79ae168b5ad961a0ae9', //Github Secret
+        'redirect' => 'http://localhost:8000/login/github/callback',
+     ],
+
+
+];
+
